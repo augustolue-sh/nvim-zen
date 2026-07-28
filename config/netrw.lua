63e8d2d0d193d9a2da491a17407fe9faa3aa1148
Augusto Lúe <augustodesigner26@gmail.com>
2026-07-28 11:27:07 -0600

    Add Netrw configuration settings

---
+++ config/netrw.lua
@@ -0,0 +1,13 @@
+-- ============================================
+-- Netrw — Explorador de archivos nativo
+-- ============================================
+
+local g = vim.g
+
+g.netrw_banner = 0
+g.netrw_liststyle = 3
+g.netrw_browse_split = 4
+g.netrw_altv = 1
+g.netrw_winsize = 25
+g.netrw_keepdir = 0
+g.netrw_localcopydircmd = 'cp -r'
